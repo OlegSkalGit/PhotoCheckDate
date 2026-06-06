a5e227e99635d1d1c537cd969d63904e1f5e5e96
OlegSkalGit <sov.develop.ua@gmail.com>
2026-06-06 14:16:44 +0300

    Restore original _START_.bat

--- _START_ .bat
+++ _START_ .bat
@@ -1,17 +1,89 @@
-п»ї@echo off
-chcp 65001 > nul
-echo РџРµСЂРµРІС–СЂРєР° СЃРµСЂРµРґРѕРІРёС‰Р°...
+@echo off
+cd /d "%~dp0"
+chcp 1251 > nul
 
-".venv\Scripts\python" -c "import PIL, pillow_heif" >nul 2>nul
-if %errorlevel% neq 0 (
-    echo [Р†РќР¤Рћ] Р’СЃС‚Р°РЅРѕРІР»РµРЅРЅСЏ Р·Р°Р»РµР¶РЅРѕСЃС‚РµР№ Сѓ РІС–СЂС‚СѓР°Р»СЊРЅРµ СЃРµСЂРµРґРѕРІРёС‰Рµ...
-    ".venv\Scripts\pip" install -r requirements.txt
-    if %errorlevel% neq 0 (
-        echo [РџРћРњРР›РљРђ] РќРµ РІРґР°Р»РѕСЃСЏ РІСЃС‚Р°РЅРѕРІРёС‚Рё Р±С–Р±Р»С–РѕС‚РµРєРё.
+setlocal enabledelayedexpansion
+
+title TemplateMachine Control Center
+echo ========================================================
+echo  TEMPLATEMACHINE CONTROL CENTER
+echo ========================================================
+echo.
+
+REM 1. Перевірка наявності Python
+python --version >nul 2>nul
+if %ERRORLEVEL% neq 0 (
+    echo Python НЕ знайдено у системі.
+    if not exist "setup\python-3.12.10-amd64.exe" (
+        echo Інсталятор Python не знайдено. Завантажуємо...
+        if not exist "setup" mkdir "setup"
+        curl -L "https://www.python.org/ftp/python/3.12.10/python-3.12.10-amd64.exe" -o "setup\python-3.12.10-amd64.exe"
+        if !ERRORLEVEL! neq 0 (
+            echo.
+            echo ===============================================================================================
+            echo Для ПЕРШОГО запуску програми необхідний інтернет для завантаження ДОДАТКОВИХ компонентів.
+            echo ===============================================================================================
+            echo.
+            echo Не вдалося завантажити Python. Перевірте з'єднання з інтернетом.
+            pause
+            exit /b 1
+        )
+    ) else (
+        echo Інсталятор Python знайдено. Встановлюємо...
+        .\setup\python-3.12.10-amd64.exe /quiet PrependPath=1 InstallAllUsers=1
+        if !ERRORLEVEL! neq 0 (
+            echo.
+            echo ===============================================================================================
+            echo Для ПЕРШОГО запуску програми необхідний інтернет для завантаження ДОДАТКОВИХ компонентів.
+            echo ===============================================================================================
+            echo.
+            echo Не вдалося встановити Python. Перевірте з'єднання з інтернетом.
+            pause
+            exit /b 1
+        ) else (
+            echo Очікування завершення встановлення...
+            timeout /t 15 > nul
+        )
+    )
+) else (
+   echo Python знайдено у системі.
+)
+
+REM 2. Створення віртуального оточення
+if not exist ".venv" (
+    echo Віртуальне оточення Python .venv у проекті відсутнє. Створюємо...
+    echo.
+    python -m venv .venv
+) else (
+    echo Віртуальне оточення Python .venv у проекті знайдено.
+)
+
+REM 3. Перевірка наявності необхідних бібліотек
+.venv\Scripts\python.exe -c "import PIL, pillow_heif" >nul 2>nul
+if %ERRORLEVEL% neq 0 (
+    echo Необхідні бібліотеки Python НЕ знайдено у проекті. Завантажуємо і встановлюємо...
+    .venv\Scripts\pip install -r requirements.txt
+    if !ERRORLEVEL! == 1 (
+        echo.
+        echo ===============================================================================================
+        echo Для ПЕРШОГО запуску програми необхідний інтернет для завантаження ДОДАТКОВИХ компонентів.
+        echo ===============================================================================================
+        echo.
+        echo [Помилка] Не вдалося встановити бібліотеки. Перевірте з'єднання з інтернетом.
         pause
         exit /b 1
     )
+) else (
+    echo Необхідні бібліотеки Python знайдено у проекті.
 )
 
-start "" ".venv\Scripts\pythonw" "main.py"
-exit
+endlocal
+
+REM 5. Запуск Streamlit
+echo.
+echo Все готово до запуску!
+echo Запуск додатку...
+echo.
+rem start ".venv\Scripts\python" "main.py"
+".venv\Scripts\python" "main.py"
+pause
